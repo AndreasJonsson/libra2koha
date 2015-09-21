d0b73aaf5076ca63b5ddf8b8677e7115b4347923
Magnus Enger <magnus@libriotech.no>
2015-09-21 15:01:17 +0200

    Comment out code related to records in libra2koha.sh, start to add borrowers

--- libra2koha.sh
+++ libra2koha.sh
@@ -36,34 +36,47 @@ echo "done"
 perl -pi -e '$/=undef; s/\r\n\r\n//g' "$DIR/utf8/Borrowers.txt"
 perl -pi -e '$/=undef; s/\r\n/\n/g'   "$DIR/utf8/Borrowers.txt"
 
-### RECORDS ###
-
 # FIXME Force the user to create necessary config files, and provide skeletons
 
-if [ ! -d "$DIR/bib/" ]; then
-    mkdir "$DIR/bib/"
-fi
-echo "Going to convert bibliographic records to MARCXML... "
-cp $EXPORTCAT $EXPORTCAT_FIXED
-perl -pi -e 's/\r\n/\n/g' "$EXPORTCAT_FIXED"
-perl -pi -e '$/=undef; s/\^\n\*000/RECORD_SEPARATOR\n*000/g' "$EXPORTCAT_FIXED"
-perl -pi -e 's/\^\n//g' "$EXPORTCAT_FIXED"
-perl -pi -e 's/RECORD_SEPARATOR/^/g' "$EXPORTCAT_FIXED"
+### RECORDS ###
 
-# FIXME Path to line2iso.pl should not be hardcoded
-perl ~/scripts/libriotools/line2iso.pl -i "$EXPORTCAT_FIXED" -x > "$MARCXML"
-echo $MARCXML
-echo "done"
+#if [ ! -d "$DIR/bib/" ]; then
+#    mkdir "$DIR/bib/"
+#fi
+#echo "Going to convert bibliographic records to MARCXML... "
+#cp $EXPORTCAT $EXPORTCAT_FIXED
+#perl -pi -e 's/\r\n/\n/g' "$EXPORTCAT_FIXED"
+#perl -pi -e '$/=undef; s/\^\n\*000/RECORD_SEPARATOR\n*000/g' "$EXPORTCAT_FIXED"
+#perl -pi -e 's/\^\n//g' "$EXPORTCAT_FIXED"
+#perl -pi -e 's/RECORD_SEPARATOR/^/g' "$EXPORTCAT_FIXED"
+
+## FIXME Path to line2iso.pl should not be hardcoded
+#perl ~/scripts/libriotools/line2iso.pl -i "$EXPORTCAT_FIXED" -x > "$MARCXML"
+#echo $MARCXML
+#echo "done"
+
+## Create tables and load the datafiles
+#echo -n "Going to create tables for records and items, and load data into MySQL... "
+#cd $SCRIPTDIR
+#perl ./create_tables.pl --dir $DIR > ./bib_tables.sql
+#mysql --local-infile -u libra2koha -ppass libra2koha < ./bib_tables.sql
+#echo "ALTER TABLE Items ADD COLUMN done INT(1) DEFAULT 0;" | mysql --local-infile -u libra2koha -ppass libra2koha
+#echo "done"
+
+## Get the relevant info out of the database and into a .marcxml file
+#echo -n "Going to transform records... "
+#perl records.pl --config $CONFIG --limit 1000 --infile $MARCXML --flag_done
+#echo "done"
+
+### BORROWERS ###
 
 # Create tables and load the datafiles
-echo -n "Going to create tables and load data into MySQL... "
-cd $SCRIPTDIR
-perl ./create_tables.pl --dir $DIR > ./tables.sql
-mysql --local-infile -u libra2koha -ppass libra2koha < ./tables.sql 
-echo "ALTER TABLE Items ADD COLUMN done INT(1) DEFAULT 0;" | mysql --local-infile -u libra2koha -ppass libra2koha
+echo -n "Going to create tables for borrowers, and load data into MySQL... "
+perl ./create_tables.pl --dir $DIR --tables Borrowers|BorrowerPhoneNumbers > ./borrowers_tables.sql
+mysql --local-infile -u libra2koha -ppass libra2koha < ./borrowers_tables.sql
 echo "done"
 
 # Get the relevant info out of the database and into a .marcxml file
 echo -n "Going to transform records... "
-perl records.pl --config $CONFIG --limit 1000 --infile $MARCXML --flag_done
+perl borrowers.pl --config $CONFIG --limit 1000
 echo "done"
