b653c0625c55129af0d02cf27bd75d8f5b4f1be4
Andreas Jonsson <andreas.jonsson@kreablo.se>
2017-09-10 21:05:32 +0200

    Update table2config.pl

--- table2config.pl
+++ table2config.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
 # Copyright 2015 Magnus Enger Libriotech
+# Copyright 2017 Andreas Jonsson andreas.jonsson@kreablo.se
 
 =head1 NAME
 
@@ -12,17 +13,23 @@ table2config.pl - Helper script for turning a table into a YAML config file.
 
 =head1 DESCRIPTION
 
-Takes three arguments:
+Arguments:
 
 =over 4
 
-=item * A path to a table file, e.g. Something.txt (not Somethingspec.txt)
+=item * --dir Data directory with table files
 
-=item * The position of the column that should be turned into the key
+=item * --name A name of a table file.  If file is not found, suffixes .txt and .csv will be tried.
 
-=item * The position of the column that should be turned into a comment
+=item * --key The index of the column that should be turned into the key
+
+=item * --comment The indicies of the columns that should be turned into a comment (0 or more)
+
+=item * --value The index of the value column (optional).
 
 =back
+
+Se --help for further options.
 
 So with this file as the starting point ("!*!" is the field separator):
 
@@ -33,7 +40,7 @@ So with this file as the starting point ("!*!" is the field separator):
 
 and this invocation of the script:
 
-  table2config.pl /path/to/Departments.txt 0 2 > ccode.yaml
+  table2config.pl --dir=/path/to --name=Departments --key=0 --comment=2 > ccode.yaml
 
 the outout will look like this:
 
@@ -52,24 +59,76 @@ be used as a config file by F<records.pl>
 
 use DateTime;
 use Modern::Perl;
+use Text::CSV;
+use Getopt::Long::Descriptive;
+use Data::Dumper;
 
-say "Usage: $0 /path/to/Sometable.txt key-index comment-index" unless $ARGV[0];
+my ($opt, $usage) = describe_options(
+    '%c %o <some-arg>',
+    [ 'dir=s', "table directory", { required => 1  } ],
+    [ 'name=s', 'table name', { required => 1 } ],
+    [ 'columndelimiter=s', 'column delimiter',  { default => '!*!' } ],
+    [ 'rowdelimiter=s',  'row delimiter'      ],
+    [ 'encoding=s',  'character encoding',      { default => 'utf-16' } ],
+    [ 'timezone=s',  'time zone' ],
+    [ 'quote=s',  'quote character' ],
+    [ 'headerrows=i', 'number of header rows',  { default => 0 } ],
+    [ 'key=i', 'index of key column', { required => 1} ],
+    [ 'value=i', 'index of value column' ],
+    [ 'comment=i@', 'index of columns to include as comments' ],
+    [],
+    [ 'verbose|v',  "print extra stuff"            ],
+    [ 'help',       "print usage message and exit", { shortcircuit => 1 } ],
+    );
 
-open (my $fh, "<:encoding(utf-16):crlf", $ARGV[0]);
+print $usage->text if ($opt->help);
 
-my $dt = DateTime->now( time_zone => 'Europe/Oslo' );
+my $filename = $opt->dir . '/' . $opt->name;
+my $fh;
+
+my $found_file = 0;
+foreach my $suffix ('', '.txt', '.csv') {
+    if (open ($fh, "<:encoding(" . $opt->encoding . "):crlf", ($filename  . $suffix))) {
+	$found_file = 1;
+    }
+}
+die ("Didn't find any table file for " . $opt->name) unless ($found_file);
+
+
+my $dt;
+if ($opt->timezone) {
+    $dt = DateTime->now( time_zone => $opt->timezone );
+} else {
+    $dt = DateTime->now();
+}
 
 say "---";
-say "# Generated from $ARGV[0]";
+say "# Generated from $filename";
 say "# " . $dt->ymd . ' ' . $dt->hms;
 say ''  ;
 
-while (my $line = <$fh> ) {
-    chomp($line);
+my $csv = Text::CSV->new({
+    quote_char => $opt->quote,
+    sep_char => $opt->columndelimiter,
+    eol => $opt->rowdelimiter });
 
-    my @fields = split /!\*!/, $line;
-    my $key     = $fields[ $ARGV[1] ];
-    my $comment = $fields[ $ARGV[2] ];
-    say "$key: '' # $comment";
+for (my $i = 0; $i < $opt->headerrows; $i++) {
+    $csv->getline( $fh );
+}
+
+
+while (my $row = $csv->getline( $fh ) ) {
+
+    my $key     = $row->[ $opt->key ];
+    my $value   = '';
+    if (defined($opt->value)) {
+	$value = $row->[$opt->value];
+    }
+    my @comments = map { $row->[$_] } @{$opt->comment};
+
+    my $comment = join(", ", @comments);
+    say "$key: '$value' # $comment";
 
 }
+
+close $fh;
